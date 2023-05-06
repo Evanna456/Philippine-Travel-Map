1d18208d467e6930d31e47bfead40e8903a0daa6
Evanna Tabs <steampunk852@yahoo.com>
2023-05-06 11:39:53 +0800

    mockup

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-lg-12" align="center">
         <h2>Philippine Travel Map</h2>
+        <p>Score: {{ score }}</p>
         <div id="map"></div>
       </div>
     </div>
@@ -18,9 +19,12 @@
 <script lang="ts">
   import { ref, defineComponent } from "vue";
   import { useMeta } from "vue-meta";
+  import $ from "jquery";
 
   import "leaflet/dist/leaflet.css";
   import leaflet from "leaflet/dist/leaflet.js";
+
+  import Swal from "sweetalert2";
 
   export default defineComponent({
     name: "Index",
@@ -31,11 +35,13 @@
     data: function () {
       return {
         map: null,
+        score: 0,
       };
     },
     mounted() {
       window.scrollTo(0, 0);
       this.mapInit();
+      this.polygonClick();
     },
     updated() {},
     computed: {},
@@ -55,28 +61,30 @@
         const response = await fetch("/Provinces.geojson");
         const provinces = await response.json();
 
-        //console.log(provinces);
-
-        var p = leaflet.geoJson(provinces).coordsToLatLngs();
-
-        leaflet
-          .geoJson(provinces, {
-            onEachFeature: function (feature, layer) {
-              console.log();
-              leaflet.GeoJson.coordsToLatLngs();
-
-              /*
-            for (var it = 0; it < 10; it++) {
-              leaflet
-                .polygon()
-                .addTo(map);
+        leaflet.geoJSON(provinces).addTo(map);
+      },
+      polygonClick() {
+        var self = this;
+        $("#map").on("click", ".leaflet-interactive", function () {
+          var poly = this;
+          Swal.fire({
+            title: "",
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: "I stayed in there",
+            denyButtonText: "I traveled in there",
+          }).then((result) => {
+            if (result.isConfirmed) {
+              $(poly).css("fill", "green");
+              $(poly).css("fill-opacity", "1");
+              self.score = self.score + 20;
+            } else if (result.isDenied) {
+              $(poly).css("fill", "yellow");
+              $(poly).css("fill-opacity", "1");
+              self.score = self.score + 10;
             }
-            */
-            },
-          })
-          .coordsToLatLngs();
-
-        //leaflet.geoJSON(provinces).addTo(map);
+          });
+        });
       },
     },
   });
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ import { createMetaManager } from "vue-meta";
           **/
 import "bootstrap/dist/css/bootstrap.min.css";
 import bootstrap from 'bootstrap/dist/js/bootstrap.min.js';
+import jquery from 'jquery/dist/jquery.min.js';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 import Index from "./components/Index.vue";
 
@@ -26,5 +30,7 @@ const router = createRouter({
 const app = createApp(App);
 app.use(router);
 app.use(createMetaManager());
+app.use(jquery);
 app.use(bootstrap);
+app.use(VueSweetalert2);
 app.mount('#app');
